7495bba38245e4a3c369ef52b2f45e75e3388017
Thyago Chatack <chatack.tc@gmail.com>
2026-02-19 18:41:59 -0300

    feat: add logic for play sound with keyboard

---
+++ script.js
@@ -0,0 +1,15 @@
+const playSound = async (e) => {
+  console.log(e);
+  
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
+  if(!audio) return;
+
+  audio.currentTime = 0;
+  audio.play();
+  
+}
+
+
+window.addEventListener('keydown', playSound)
